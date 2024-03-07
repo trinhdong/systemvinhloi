59f0d1b522b8c9ceb45ee2a1c537945e23115603
Hiếu Nguyễn <hieu_nld@vietvang.net>
2024-03-07 20:45:18 +0700

    Updated model

---
+++ app/Models/Area.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Area extends Model
+{
+    use HasFactory;
+    protected $table = 'areas';
+    protected $fillable = [
+        'name',
+        'description',
+        'created_by',
+        'updated_by',
+        'created_at',
+        'updated_at',
+    ];
+}
---
+++ app/Models/User.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_code',
+        'name',
+        'email',
+        'role',
+        'is_admin',
+        'email_verified_at',
+        'password',
+        'created_by',
+        'updated_by',
+        'remember_token',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+}
